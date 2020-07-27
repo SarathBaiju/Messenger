21bbb505c757bc509946d0ec549be769c5513d6f
Sarath baiju <sarath.bu@pitsolutions.com>
2020-07-27 16:22:08 +0530

    client app s

--- MessengerUI/Controllers/HomeController.cs
+++ MessengerUI/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MessengerUI.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page."; 
 
             return View();
         }
